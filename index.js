63cd7175d1f78e4a5c8308f9b5f0e6aa07e75e80
saimRamzan <saimshinwari001@gmail.com>
2024-01-01 12:50:18 +0500

    all have good chagnes in lower to high if score = 0 to reset game advance changing

--- index.js
+++ index.js
@@ -1,40 +1,55 @@
 let button = document.getElementById('button');
 let resetButton = document.getElementById('resetGame');
 document.getElementById('lowHigh').innerHTML = `Try Guess 🤔`
-let counter = 0
+let counter = 3
 let score= document.getElementById('scores');
+
 // Random Number Gernator
 let secretNumber = Math.trunc(Math.random() * 15) + 1
 console.log(secretNumber);
+
 // game button work
 button.addEventListener('click',()=>{
     let input = document.getElementById('input').value
     document.getElementById('guessNumberAre').innerHTML = `Guessed number are: ${input}`;
+
 // input in Empty
     if (!input) {
         document.getElementById('lowHigh').innerHTML = 'Input Empty 😡'
+
 // correct Guess & reset
     }else if (input == secretNumber){
-            document.getElementById('lowHigh').innerHTML = 'You guess is Correct 🥳'
-            counter = 0;
+            document.getElementById('lowHigh').innerHTML = 'Congratulations 🥳'
+            counter = 3;
             score.textContent = counter;
-            setTimeout(() => {
-                    document.getElementById('input').value = '';
-                    document.getElementById('guessNumberAre').innerHTML = `Guessed number are: 0`;
-                    secretNumber = Math.trunc(Math.random() * 15) + 1
-                    console.log(secretNumber);
-                    document.getElementById('lowHigh').innerHTML = `Try Guess 🤔`
-            }, 1000);
+            reset()
+
 // input grater to secret number
     }else if (parseInt(input) > secretNumber){
         document.getElementById('lowHigh').innerHTML ='You guess is too High 👆';
-        counter++;
-            score.textContent = counter;
+        counter--; 
+           let count = score.textContent = counter;
+           if (count === 0) {
+            document.getElementById('lowHigh').innerHTML ='Game Over 🤙'
+            reset()
+           }
 // Input lower to secret number
     }else if (parseInt(input) < secretNumber){
         document.getElementById('lowHigh').innerHTML ='You guess is too Low 👇'
-        counter++;
-        score.textContent = counter;
-    }
-})
+        counter--; 
+           let count = score.textContent = counter;
+           if (count === 0) {
+            document.getElementById('lowHigh').innerHTML ='Game Over 🤙'
+            reset()
+           }
+}})
+let reset  = ()=>{ setTimeout(() => {
+    document.getElementById('input').value = '';
+    document.getElementById('guessNumberAre').innerHTML = `Guessed number are: 0`;
+    secretNumber = Math.trunc(Math.random() * 15) + 1
+    counter = 3;
+    document.getElementById('scores').innerHTML = 3;
 
+    console.log(secretNumber);
+    document.getElementById('lowHigh').innerHTML = `Try Guess 🤔`
+}, 2000)};
